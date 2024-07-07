74ceaf59aee96f2c30ca0811d096cc281844fd41
Van Tho Tran <thotv.t1.1821@gmail.com>
2024-07-08 00:59:03 +0700

    add authen to / route

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,12 +1,13 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { authRouter } from './auth.route';
 import { errorRouter } from './error.route';
+import authentication from '../middlewares/authentication';
 
 const router = express.Router();
 
 router.use('/', authRouter);
 router.use('/', errorRouter);
-router.get('/', (req: Request, res: Response, next: NextFunction) => {
+router.get('/', authentication, (req: Request, res: Response, next: NextFunction) => {
   res.render('./dashboard');
 });
 router.use((req: Request, res: Response, next: NextFunction) => {
